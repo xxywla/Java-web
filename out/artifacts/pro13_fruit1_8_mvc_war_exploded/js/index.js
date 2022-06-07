0a18faabc2e9825648b3aa401f8dfbc3aa68a1ee
qrt <xxywqrt@qq.com>
2022-06-07 15:10:25 +0800

    update .gitignore

--- out/artifacts/pro13_fruit1_8_mvc_war_exploded/js/index.js
+++
@@ -1,9 +0,0 @@
-function delFruit(fid) {
-    if (confirm("是否删除该水果")) {
-        window.location.href = 'fruit.do?fid=' + fid + '&operator=del';
-    }
-}
-
-function page(pageNum) {
-    window.location.href = 'fruit.do?pageNum=' + pageNum;
-}
