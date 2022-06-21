20a88d058600d49b75cc3e2b1e8cd6d852e31ae8
xxywla <xxywqrt@qq.com>
2022-06-21 19:29:25 +0800

    在detail.html中提交回复

--- pro21-qqzone1.0/src/com/xxyw/qqzone/service/ReplyService.java
+++ pro21-qqzone1.0/src/com/xxyw/qqzone/service/ReplyService.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface ReplyService {
     // 根据 TopicId 获取对应的回复列表
     List<Reply> getReplyListByTopicId(Integer topicId);
+
+    // 增加对某个 topic 的回复
+    void addReply(Reply reply);
 }
