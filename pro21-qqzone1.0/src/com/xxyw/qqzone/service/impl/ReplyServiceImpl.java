a5f4da561e8b447e41e2770b1e7711d3e5a537fa
xxywla <xxywqrt@qq.com>
2022-06-21 15:16:58 +0800

    显示detail.html页面的topic、回复、主人回复

--- pro21-qqzone1.0/src/com/xxyw/qqzone/service/impl/ReplyServiceImpl.java
+++ pro21-qqzone1.0/src/com/xxyw/qqzone/service/impl/ReplyServiceImpl.java
@@ -4,20 +4,27 @@ import com.xxyw.qqzone.dao.ReplyDAO;
 import com.xxyw.qqzone.pojo.HostReply;
 import com.xxyw.qqzone.pojo.Reply;
 import com.xxyw.qqzone.pojo.Topic;
+import com.xxyw.qqzone.pojo.UserBasic;
 import com.xxyw.qqzone.service.HostReplyService;
 import com.xxyw.qqzone.service.ReplyService;
+import com.xxyw.qqzone.service.UserBasicService;
 
 import java.util.List;
 
 public class ReplyServiceImpl implements ReplyService {
     ReplyDAO replyDAO = null;
     HostReplyService hostReplyService = null;
+    UserBasicService userBasicService = null;
 
     @Override
     public List<Reply> getReplyListByTopicId(Integer topicId) {
         List<Reply> replyList = replyDAO.getReplyList(new Topic(topicId));
         for (int i = 0; i < replyList.size(); i++) {
             Reply reply = replyList.get(i);
+
+            UserBasic author = reply.getAuthor();
+            author = userBasicService.getUserBasicById(author.getId());
+            reply.setAuthor(author);
 
             HostReply hostReply = hostReplyService.getHostReplyByReplyId(reply.getId());
 
--- pro21-qqzone1.0/src/com/xxyw/qqzone/service/impl/TopicServiceImpl.java
+++ pro21-qqzone1.0/src/com/xxyw/qqzone/service/impl/TopicServiceImpl.java
@@ -6,12 +6,14 @@ import com.xxyw.qqzone.pojo.Topic;
 import com.xxyw.qqzone.pojo.UserBasic;
 import com.xxyw.qqzone.service.ReplyService;
 import com.xxyw.qqzone.service.TopicService;
+import com.xxyw.qqzone.service.UserBasicService;
 
 import java.util.List;
 
 public class TopicServiceImpl implements TopicService {
     private TopicDAO topicDAO = null;
     private ReplyService replyService = null;
+    private UserBasicService userBasicService = null;
 
     @Override
     public List<Topic> getTopicList(UserBasic userBasic) {
@@ -21,6 +23,12 @@ public class TopicServiceImpl implements TopicService {
     @Override
     public Topic getTopicById(Integer topicId) {
         Topic topic = topicDAO.getTopic(topicId);
+
+        // 查到的只有作者的 id 需要根据 id 获取作者的其他属性
+        UserBasic author = topic.getAuthor();
+        author = userBasicService.getUserBasicById(author.getId());
+
+        topic.setAuthor(author);
 
         List<Reply> replyList = replyService.getReplyListByTopicId(topicId);
 
